665834b943fe2b49831d03d2d5b671e9db457277
pavel <gitkornienkov@gmail.com>
2023-06-14 23:45:02 +0300

    Full code

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     static String[] products = {"Хлеб", "Яблоки", "Молоко"};
     static int[] prices = {100, 200, 300};
-    static int quantity[] = new int[3];
+    static int[] quantity = new int[3];
 
     public static void main(String[] args) {
         System.out.println("Товары для покупки: ");
@@ -14,33 +15,32 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        int sumProducts = 0;
-
         while (true) {
-            int productNumber = 0;
-            int productCount = 0;
 
             System.out.println("Выберите товар и количество через пробел или введите `end` для завершения");
 
             String input = scanner.nextLine();
 
             if ("end".equals(input)) {
-                System.out.println("");
+                System.out.println("Программа завершена!");
                 break;
             }
             String[] pairNumbQuantity = input.split(" ");
             int productNumb = Integer.parseInt(pairNumbQuantity[0]);
             int productQuantity = Integer.parseInt(pairNumbQuantity[1]);
-
             quantity[productNumb - 1] = productQuantity;
 
-
+            int sumProducts = 0;
             System.out.println("Ваша корзина");
             for (int i = 0; i < products.length; i++) {
-                System.out.println(products[i] + " - " + quantity[i] + " руб за шт/кг");
+                System.out.println(products[i] + " - " + quantity[i] + " шт." + " - " + (quantity[i] * prices[i]) + " руб");
+
+                sumProducts += (quantity[i] * prices[i]);
+
+
             }
-
-
+            System.out.println("Итого: " + sumProducts + " руб.");
         }
+
     }
 }
