be8e05017de4d42adafbaacffaf80f3d77e88f19
caprikorp <pikornienkov@gmail.com>
2023-06-16 20:10:07 +0300

    Add skip zero product

--- src/Main.java
+++ src/Main.java
@@ -12,25 +12,35 @@ public class Main {
         for (int i = 0; i < products.length; i++) {
             System.out.println(products[i] + " - " + prices[i] + " руб за шт/кг");
         }
+
         Scanner scanner = new Scanner(System.in);
+
         while (true) {
+
             System.out.println("Выберите товар и количество через пробел или введите `end` для завершения");
+
             String input = scanner.nextLine();
-            
+
             if ("end".equals(input)) {
                 System.out.println("Программа завершена!");
                 break;
             }
-            
+
             String[] pairNumbQuantity = input.split(" ");
+
             int productNumb = Integer.parseInt(pairNumbQuantity[0]);
             int productQuantity = Integer.parseInt(pairNumbQuantity[1]);
-            quantity[productNumb - 1] = productQuantity;
-            
             int sumProducts = 0;
+
+
+            quantity[productNumb - 1] = productQuantity;
+
             System.out.println("Ваша корзина");
+
             for (int i = 0; i < products.length; i++) {
-                System.out.println(products[i] + " - " + quantity[i] + " шт." + " - " + (quantity[i] * prices[i]) + " руб");
+                if (quantity[i] != 0) {
+                    System.out.println(products[i] + " - " + quantity[i] + " шт." + " - " + (quantity[i] * prices[i]) + " руб");
+                }
                 sumProducts += (quantity[i] * prices[i]);
             }
             System.out.println("Итого: " + sumProducts + " руб.");
