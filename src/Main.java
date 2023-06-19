2f918f2e386b841a4d3cdf7ad9aebf4234072672
caprikorp <pikornienkov@gmail.com>
2023-06-19 11:07:54 +0300

    Удалил лишние переменые, пробелы, форматировал код

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.Scanner;
 import java.util.stream.IntStream;
 
@@ -8,53 +7,33 @@ public class Main {
     static int[] quantity = new int[3];
     static int[] sumProducts = new int[3];
 
-
     public static void main(String[] args) {
-        int sum = 0;
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("Товары для покупки: ");
-
         for (int i = 0; i < products.length; i++) {
             System.out.println((i + 1) + ". " + products[i] + " - " + prices[i] + " руб за шт/кг");
         }
 
-        Scanner scanner = new Scanner(System.in);
-
         while (true) {
-
             System.out.println("Выберите товар и количество через пробел или введите `end` для завершения");
-
             String input = scanner.nextLine();
 
             if ("end".equals(input)) {
                 System.out.println("Программа завершена!");
                 break;
             }
-
             String[] pairNumbQuantity = input.split(" "); //Сплит
-
-            int productNumb = Integer.parseInt(pairNumbQuantity[0]) - 1;
-            int productQuantity = Integer.parseInt(pairNumbQuantity[1]);
-
-            quantity[productNumb] = quantity[productNumb] + productQuantity;
+            quantity[(Integer.parseInt(pairNumbQuantity[0]) - 1)] = quantity[(Integer.parseInt(pairNumbQuantity[0]) - 1)] + Integer.parseInt(pairNumbQuantity[1]);
         }
 
         System.out.println("Ваша корзина");
-
         for (int i = 0; i < products.length; i++) {
-
             if (quantity[i] != 0) {
                 System.out.println(products[i] + " - " + quantity[i] + " шт." + " - " + (quantity[i] * prices[i]) + " руб");
             }
             sumProducts[i] = quantity[i] * prices[i];
-
-            for (int y = 0; y < sumProducts.length; y++) {
-                sum = IntStream.of(sumProducts).sum(); //Суммирует элементы массива.
-
-            }
-
         }
-        System.out.println("Итого: " + sum + " руб.");
+        System.out.println("Итого: " + IntStream.of(sumProducts).sum() + " руб.");
     }
-
 }
